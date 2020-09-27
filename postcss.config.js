5bcc1ee25dd48c1f603cd076d0ae020e2477d17e
Mattia Astorino <astorino.mattia@gmail.com>
2020-09-27 15:31:12 +0200

    chore: update postcss plugins

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    require('postcss-easy-import')({
+      extensions: ['.css']
+    }),
+    require('postcss-preset-env')({
+      stage: 0,
+    }),
+    require('cssnano')({
+      preset: [
+        'advanced',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+          reduceIdents: false,
+        },
+      ],
+    }),
+    require('postcss-reporter')({
+      clearReportedMessages: true,
+    }),
+  ]
+}
