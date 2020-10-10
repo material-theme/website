59807a20a9ca47fe1cb2246e7858e976759794a0
LasaleFamine <info@godev.space>
2020-10-10 12:31:57 +0200

    feat: getting all members and order correctly

--- api/collective.js
+++ api/collective.js
@@ -2,7 +2,17 @@ import fetch from 'isomorphic-fetch';
 
 export default async (req, res) => {
   const info = await (await fetch('https://opencollective.com/material-theme.json')).json();
-  const members = await (await fetch('https://opencollective.com/material-theme/members.json?limit=3&offset=0')).json();
+  const golds = await (await fetch('https://opencollective.com/material-theme/tiers/gold-sponsor/all.json')).json();
+  const silvers = await (await fetch('https://opencollective.com/material-theme/tiers/silver-sponsor/all.json')).json();
+  const backers = await (await fetch('https://opencollective.com/material-theme/tiers/backer/all.json')).json();
+  const donations = await (await fetch('https://opencollective.com/material-theme/tiers/donation/all.json')).json();
+
+  const members = [
+    ...donations,
+    ...backers,
+    ...silvers,
+    ...golds,
+  ].slice(-8);
 
   res.json({
     info,
--- src/assets/js/components/contributors.js
+++ src/assets/js/components/contributors.js
@@ -10,9 +10,15 @@ export default class extends Controller {
   async connect() {
     const { info, members } = await (await fetch('/api/collective')).json();
     this.countTarget.textContent = info.contributorsCount;
-    this.avatarsTarget.querySelectorAll('img').forEach((img, index) => {
-      // eslint-disable-next-line no-param-reassign
-      img.src = members[index].image;
+    members.forEach((member) => {
+      const imgEL = document.createElement('img');
+      imgEL.setAttribute('style', '--padding:4px; --obj-f:cover; --obj-p:center; --radius:100%; --overflow:hidden; --bg-c:var(--ne-global-background);');
+      imgEL.setAttribute('loading', 'lazy');
+      imgEL.setAttribute('alt', '');
+      imgEL.setAttribute('height', 60);
+      imgEL.setAttribute('width', 60);
+      imgEL.src = member.image;
+      this.avatarsTarget.prepend(imgEL);
     });
   }
 }
