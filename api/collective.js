b087ebbc789fe6c0d2164baafe3c75808c45bf56
LasaleFamine <info@godev.space>
2020-10-10 12:54:44 +0200

    chore: removing triple member

--- api/collective.js
+++ api/collective.js
@@ -7,12 +7,29 @@ export default async (req, res) => {
   const backers = await (await fetch('https://opencollective.com/material-theme/tiers/backer/all.json')).json();
   const donations = await (await fetch('https://opencollective.com/material-theme/tiers/donation/all.json')).json();
 
+  const oneOf = {
+    bonusfinder: {
+      label: 'Bonusfinder',
+      found: false,
+    },
+  };
+
   const members = [
     ...golds,
     ...silvers,
     ...backers,
     ...donations,
-  ].slice(0, 8);
+  ]
+    .filter((member) => {
+      const isBonusFinder = member.name.includes(oneOf.bonusfinder);
+      if (isBonusFinder && oneOf.bonusfinder.found) {
+        return false;
+      }
+
+      oneOf.bonusfinder.found = isBonusFinder;
+      return true;
+    })
+    .slice(0, 8);
 
   res.json({
     info,
