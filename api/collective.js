baf191976549cb0e7373751e61b3de1690cd9215
Alessio Occhipinti <LasaleFamine@users.noreply.github.com>
2020-10-10 14:37:40 +0200

    Feat/complete collective (#5)
    * feat: getting all members and order correctly
    
    * chore: first gold sponsors
    
    * chore: remove useless reverse
    
    * chore: correct slice
    
    * chore: removing triple member
    
    * chore: correct filter on label
    
    * chore: cleanup comments

--- api/collective.js
+++ api/collective.js
@@ -2,7 +2,34 @@ import fetch from 'isomorphic-fetch';
 
 export default async (req, res) => {
   const info = await (await fetch('https://opencollective.com/material-theme.json')).json();
-  const members = await (await fetch('https://opencollective.com/material-theme/members.json?limit=3&offset=0')).json();
+  const golds = await (await fetch('https://opencollective.com/material-theme/tiers/gold-sponsor/all.json')).json();
+  const silvers = await (await fetch('https://opencollective.com/material-theme/tiers/silver-sponsor/all.json')).json();
+  const backers = await (await fetch('https://opencollective.com/material-theme/tiers/backer/all.json')).json();
+  const donations = await (await fetch('https://opencollective.com/material-theme/tiers/donation/all.json')).json();
+
+  const oneOf = {
+    bonusfinder: {
+      label: 'bonusfinder',
+      found: false,
+    },
+  };
+
+  const members = [
+    ...golds,
+    ...silvers,
+    ...backers,
+    ...donations,
+  ]
+    .filter((member) => {
+      const isBonusFinder = member.name.toLowerCase().includes(oneOf.bonusfinder.label);
+      if (isBonusFinder && oneOf.bonusfinder.found) {
+        return false;
+      }
+
+      oneOf.bonusfinder.found = isBonusFinder;
+      return true;
+    })
+    .slice(0, 8);
 
   res.json({
     info,
